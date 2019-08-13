b4e4ecec14a33aa6a206fc520026da0db980860a
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-13 23:22:41 +0700

    Update gulp task merge-sql-scripts

--- gulpfile.js
+++ gulpfile.js
@@ -86,8 +86,8 @@ gulp.task('bundle-js', () => {
 
 gulp.task('merge-sql-scripts', () => {
     let task = new GulpFileMerge();
-    let sDate = '2019-04-26';
-    let rootPath = path.join(__dirname, 'src/server/db/scripts/' + sDate);
+    let sDate = '2019-08-15';
+    let rootPath = path.join(__dirname, 'db/scripts/' + sDate);
     task.opts = {
         src: [
             /* Concat all *.sql file in subdirectories. */
