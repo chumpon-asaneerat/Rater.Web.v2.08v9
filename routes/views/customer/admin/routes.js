18325f19f577a10b1789c96554919b1ecc01558c
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-22 21:42:36 +0700

    Update 2019-08-22 #15 ===================== ## Server    - Update todo list #4    - fixed script reference (app.js) in all index.html files.

--- routes/views/customer/admin/routes.js
+++ routes/views/customer/admin/routes.js
@@ -51,7 +51,7 @@ router.get('/', routes.home)
 router.get('/:file', routes.getfile)
 
 const init_routes = (svr) => {
-    svr.route('/admin', router);
+    svr.route('/customer/admin', router);
 };
 
 module.exports.init_routes = exports.init_routes = init_routes;
--- routes/views/customer/device/routes.js
+++ routes/views/customer/device/routes.js
@@ -51,7 +51,7 @@ router.get('/', routes.home)
 router.get('/:file', routes.getfile)
 
 const init_routes = (svr) => {
-    svr.route('/device', router);
+    svr.route('/customer/device', router);
 };
 
 module.exports.init_routes = exports.init_routes = init_routes;
