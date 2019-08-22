7883364c6ac9623ddddc627a9148f41ded027158
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-22 21:22:28 +0700

    Update 2019-08-22 #12 ===================== ## Server  # edl routes    - add supervisor routes basic folders and files.

---
+++ routes/views/edl/supervisor/routes.js
@@ -0,0 +1,57 @@
+//#region common requires
+
+const path = require('path');
+const rootPath = process.env['ROOT_PATHS'];
+
+// for production
+const nlibPath = path.join(rootPath, 'nlib');
+// for nlib-server dev project
+//const nlibPath = path.join(rootPath, 'src', 'server', 'js', 'nlib');
+const nlibjs = path.join(nlibPath, 'nlib');
+const nlib = require(nlibjs);
+
+const nlibExprjs = path.join(nlibPath, 'nlib-express');
+
+const WebServer = require(nlibExprjs);
+
+//#endregion
+
+//#region router type and variables
+
+const WebRouter = WebServer.WebRouter;
+const router = new WebRouter();
+
+//#endregion
+
+const routes = class {
+    /**
+     * home
+     * @param {Request} req The Request.
+     * @param {Response} res The Response.
+     */
+    static home(req, res) {
+        WebServer.sendFile(req, res, __dirname, 'index.html');
+    }
+    /**
+     * getfile
+     * @param {Request} req The Request.
+     * @param {Response} res The Response.
+     */
+    static getfile(req, res) {
+        let file = req.params.file.toLowerCase();
+        let files = ['app.js']
+        let idx = files.indexOf(file);
+        if (idx !== -1) {
+            WebServer.sendFile(req, res, __dirname, files[idx]);
+        }
+    }
+}
+
+router.get('/', routes.home)
+router.get('/:file', routes.getfile)
+
+const init_routes = (svr) => {
+    svr.route('/edl/supervisor', router);
+};
+
+module.exports.init_routes = exports.init_routes = init_routes;
