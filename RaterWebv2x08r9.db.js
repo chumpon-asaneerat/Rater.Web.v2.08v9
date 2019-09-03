a52fa5bda1690568fa374d6f4f49c225826ec4d4
chumpon-asaneerat <chumponsenate@gmail.com>
2019-09-03 14:37:09 +0700

    Update db access code (generate).

--- RaterWebv2x08r9.db.js
+++ RaterWebv2x08r9.db.js
@@ -15,8 +15,326 @@ const RaterWebv2x08r9 = class extends SqlServer {
         await super.disconnect();
     }
 
+    async GetRandomCode(pObj) {
+        let name = 'GetRandomCode';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetErrorMsg(pObj) {
+        let name = 'GetErrorMsg';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveErrorMsg(pObj) {
+        let name = 'SaveErrorMsg';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveErrorMsgML(pObj) {
+        let name = 'SaveErrorMsgML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetErrorMsgs(pObj) {
+        let name = 'GetErrorMsgs';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveLanguage(pObj) {
+        let name = 'SaveLanguage';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async DisableLanguage(pObj) {
+        let name = 'DisableLanguage';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async EnableLanguage(pObj) {
+        let name = 'EnableLanguage';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
     async GetLanguages(pObj) {
         let name = 'GetLanguages';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SavePeriodUnit(pObj) {
+        let name = 'SavePeriodUnit';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SavePeriodUnitML(pObj) {
+        let name = 'SavePeriodUnitML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetPeriodUnits(pObj) {
+        let name = 'GetPeriodUnits';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveLimitUnit(pObj) {
+        let name = 'SaveLimitUnit';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveLimitUnitML(pObj) {
+        let name = 'SaveLimitUnitML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetLimitUnits(pObj) {
+        let name = 'GetLimitUnits';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetMemberTypes(pObj) {
+        let name = 'GetMemberTypes';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveLicenseType(pObj) {
+        let name = 'SaveLicenseType';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveLicenseTypeML(pObj) {
+        let name = 'SaveLicenseTypeML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetLicenseTypes(pObj) {
+        let name = 'GetLicenseTypes';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveLicenseFeature(pObj) {
+        let name = 'SaveLicenseFeature';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetLicenseFeatures(pObj) {
+        let name = 'GetLicenseFeatures';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetLicenses(pObj) {
+        let name = 'GetLicenses';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveUserInfo(pObj) {
+        let name = 'SaveUserInfo';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveUserInfoML(pObj) {
+        let name = 'SaveUserInfoML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetUserInfos(pObj) {
+        let name = 'GetUserInfos';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveCustomer(pObj) {
+        let name = 'SaveCustomer';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveCustomerML(pObj) {
+        let name = 'SaveCustomerML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetCustomers(pObj) {
+        let name = 'GetCustomers';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async DeleteCustomer(pObj) {
+        let name = 'DeleteCustomer';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveMemberInfo(pObj) {
+        let name = 'SaveMemberInfo';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveMemberInfoML(pObj) {
+        let name = 'SaveMemberInfoML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetMemberInfos(pObj) {
+        let name = 'GetMemberInfos';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveBranch(pObj) {
+        let name = 'SaveBranch';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveBranchML(pObj) {
+        let name = 'SaveBranchML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetBranchs(pObj) {
+        let name = 'GetBranchs';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveOrg(pObj) {
+        let name = 'SaveOrg';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveOrgML(pObj) {
+        let name = 'SaveOrgML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetOrgs(pObj) {
+        let name = 'GetOrgs';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveQSet(pObj) {
+        let name = 'SaveQSet';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveQSetML(pObj) {
+        let name = 'SaveQSetML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetQSets(pObj) {
+        let name = 'GetQSets';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveQSlide(pObj) {
+        let name = 'SaveQSlide';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveQSlideML(pObj) {
+        let name = 'SaveQSlideML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetQSlides(pObj) {
+        let name = 'GetQSlides';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveQSlideItem(pObj) {
+        let name = 'SaveQSlideItem';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveQSlideItemML(pObj) {
+        let name = 'SaveQSlideItemML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetQSlideItems(pObj) {
+        let name = 'GetQSlideItems';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveDevice(pObj) {
+        let name = 'SaveDevice';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveDeviceML(pObj) {
+        let name = 'SaveDeviceML';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetDevices(pObj) {
+        let name = 'GetDevices';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetDeviceTypes(pObj) {
+        let name = 'GetDeviceTypes';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SaveVote(pObj) {
+        let name = 'SaveVote';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetVoteSummaries(pObj) {
+        let name = 'GetVoteSummaries';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetRawVotes(pObj) {
+        let name = 'GetRawVotes';
         let proc = schema[name];
         return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
     }
@@ -29,6 +347,30 @@ const RaterWebv2x08r9 = class extends SqlServer {
 
     async CheckUsers(pObj) {
         let name = 'CheckUsers';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SignIn(pObj) {
+        let name = 'SignIn';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async CheckAccess(pObj) {
+        let name = 'CheckAccess';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async GetAccessUser(pObj) {
+        let name = 'GetAccessUser';
+        let proc = schema[name];
+        return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
+    }
+
+    async SignOut(pObj) {
+        let name = 'SignOut';
         let proc = schema[name];
         return await this.execute(name, pObj, proc.parameter.inputs, proc.parameter.outputs);
     }
