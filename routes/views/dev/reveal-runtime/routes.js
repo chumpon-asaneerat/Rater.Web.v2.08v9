518288fe58b09faca74cec4e046e60035a4745b5
chumpon-asaneerat <chumponsenate@gmail.com>
2019-10-07 06:43:43 +0700

    Dev - Test Fabric.js with Reveal.js

--- routes/views/dev/reveal-runtime/routes.js
+++ routes/views/dev/reveal-runtime/routes.js
@@ -42,38 +42,12 @@ const routes = class {
     static home(req, res) {
         WebServer.sendFile(req, res, __dirname, 'index.html');
     }
-    /**
-     * getjsfile
-     * @param {Request} req The Request.
-     * @param {Response} res The Response.
-     */
-    static getjsfile(req, res) {
-        let file = req.params.file.toLowerCase();
-        let files = ['app.js']
-        let idx = files.indexOf(file);
-        if (idx !== -1) {
-            let fname = path.join(__dirname, 'js', files[idx]);
-            WebServer.sendFile(req, res, fname);
-        }
-    }
-    static getContents(req, res) {
-        let contentPath = path.join(__dirname, 'contents');
-        let folders = getDirectories(contentPath);
-        let json = {}
-        folders.forEach(dir => {
-            let langId = dir.replace(contentPath + '\\', '')
-            json[langId] = JSON.parse(fs.readFileSync(path.join(dir, 'content.json'), 'utf8'))
-        })
-        WebServer.sendJson(req, res, nlib.NResult.data(json));
-    }
 }
 
 router.get('/', routes.home)
-router.get('/contents', routes.getContents)
-router.get('/js/:file', routes.getjsfile)
 
 const init_routes = (svr) => {
-    svr.route('/dev/home', router);
+    svr.route('/dev/reveal-runtime', router);
 };
 
 module.exports.init_routes = exports.init_routes = init_routes;
